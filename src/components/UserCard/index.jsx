bd47a4a11fa40be504aac185d40fb818fcc572aa
Sareli <sareli89@users.noreply.github.com>
2022-03-07 00:58:08 -0500

    Set rounded, purple cards and padding

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -2,7 +2,7 @@
 export const UserCard = ({title, firstName, lastName, picture}) => {
     const fullName = `${firstName} ${lastName}`
     return (
-        <div className="flex items-center">
+        <div className="flex items-center bg-purple-300 rounded-lg p-4 m-2">
             <img className="rounded-full"  src={picture} alt={`Profile photo for ${fullName}`} />
             <h2 className="text-3xl font-bold underline">{title}
             {fullName}

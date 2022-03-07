447a37f8c21c4a6f4c5112c1382150ddf7970c89
Sareli <sareli89@users.noreply.github.com>
2022-03-07 01:26:07 -0500

    Fix and ware up fetch API

--- src/App.js
+++ src/App.js
@@ -3,34 +3,26 @@ import { UserCard } from './components/UserCard'
 import { getUser } from './utils/fetching';
 import { useEffect } from 'react';
 import './index.css';
+import React from 'react';
 
 function App() {
+  const [users, setUsers] = React.useState([]);
   useEffect(() => {
-    getUser();
+    async function fetchUsers() {
+      const usersResponse = await getUser();
+      const usersData = usersResponse.data;
+      setUsers(usersData);
+      console.log(usersData)
+    }
+    fetchUsers();
   }, []);
   return (
     <div className="container">
       <div className="grid gap-4 grid-cols-2 grid-rows-5">
-      
         <div className="contents">
-        <UserCard 
-          title='Mr ' 
-          firstName="Luis " 
-          lastName="Lopez"
-          picture="https://randomuser.me/api/portraits/men/30.jpg"
-          />
-          <UserCard 
-          title='Mr ' 
-          firstName="Luis " 
-          lastName="Lopez"
-          picture="https://randomuser.me/api/portraits/men/30.jpg"
-          />
-          <UserCard 
-          title='Mr ' 
-          firstName="Luis " 
-          lastName="Lopez"
-          picture="https://randomuser.me/api/portraits/men/30.jpg"
-          />
+        {users && users.map(({title, firstName, lastName, picture}, userIndex) => (
+          <UserCard key={`user-${userIndex}`} title={title} firstName={firstName} lastName={lastName} picture={picture} />
+        ))}
         </div>
       </div>
           
--- src/utils/fetching.js
+++ src/utils/fetching.js
@@ -1,9 +1,9 @@
-export const getUser = () => {
+export async function getUser () {
+    let userData;
     const headers = {'app-id': '62244f196db59039f13b12fb'};
     const users = fetch('https://dummyapi.io/data/v1/user?limit=10', {headers});
-    users.then((response) => {
-        const respnoseJson = response.json().then((value) => {
-            console.log(value);
-        })
-    })
+    userData = await (await users).json();
+   // userData = users.then(response => response.json().then().data)//.then(jsonResponse => jsonResponse.data);
+    console.log({userData})
+    return userData;
 }

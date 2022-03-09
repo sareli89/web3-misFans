9fd257b39ffe111b79c31e7352036a8ac363e1d1
Sareli <sareli89@users.noreply.github.com>
2022-03-08 21:55:48 -0500

    add comments card

---
+++ src/utils/comments.fetching.js
@@ -0,0 +1,9 @@
+// export async function getComments () {
+
+//     let commentData;
+//     const headers = {'app-id': '62244f196db59039f13b12fb'};
+//     const comments = fetch('https://dummyapi.io/data/v1/comment?limit=10', {headers});
+//     commentData = await (await comments).json();
+//     return commentData;
+//     console.log(commentData)
+// }

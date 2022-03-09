8794a360bd7b5588f708d7a45b419288e7865527
Sareli <sareli89@users.noreply.github.com>
2022-03-08 22:24:09 -0500

    font styles

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,16 +1,16 @@
 export const Nav = () => {
     return (
-        <nav  className="flex items-center justify-between flex-wrap bg-gray-900 p-6 border-b-4  border-purple-700">
+        <nav  className="flex items-center justify-between flex-wrap bg-black p-6 border-b-4  border-purple-700">
             <div className="flex items-center flex-shrink-0 text-white font-extrabold mr-6">
-                <span className="font-semibold text-xl tracking-tight">Mis Fans</span>
+                <span className="font-semibold font-mono text-xl tracking-tight">Mis Fans</span>
             </div>
             <div className="input-group">
-                <input type="search" className="input-group"></input>
+                <input type="search" className="input-group min-w-0 block w-full px-3 py-1.5 font-mono" placeholder="Search" aria-label="Search"></input>
 
             </div>
             
             <div>
-                <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white font-semibold border-purple-500 hover:border-transparent hover:text-purple-300 hover:bg-purple-600 mt-4 lg:mt-0">Registrarme</a>
+                <a href="#" className="inline-block text-sm font-mono px-4 py-2 leading-none border rounded text-white font-semibold border-purple-500 hover:border-transparent hover:text-purple-300 hover:bg-purple-600 mt-4 lg:mt-0">Registrarme</a>
             </div>
            
         </nav>
--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -7,14 +7,14 @@ export  const UserProfile = ({user: {picture, firstName}}) => {
             <div className="  bg-black ">
                 <div className="flex items-center">
                     <img width="90" className="rounded-full" src={picture} alt={profilePhotoAlt}/>
-                    <h2 className="text-white font-semibold mx-8 ">Hola, soy {firstName}</h2>
+                    <h2 className="text-white text-xl font-semibold mx-8 ">Hola, soy {firstName}</h2>
                 </div>
                 
 
                 <div className="flex justify-center my-8 card">
                     <div className="block p-6 rounded-lg shadow-lg border hover:border-doted border-fuchsia-50 bg-purple-400  max-w-sm">
-                        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
-                        <p className="text-gray-700 text-base mb-4">
+                        <h5 className="font-mono text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
+                        <p className="font-mono text-gray-700 text-base mb-4">
                         Comment
                         </p>
                     </div>
@@ -22,16 +22,16 @@ export  const UserProfile = ({user: {picture, firstName}}) => {
 
                 <div className="flex justify-center my-8 card">
                     <div className="block p-6 rounded-lg shadow-lg border hover:border-doted border-fuchsia-50   bg-purple-400  max-w-sm">
-                        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
-                        <p className="text-gray-700 text-base mb-4">
+                        <h5 className="font-mono text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
+                        <p className="font-mono text-gray-700 text-base mb-4">
                         Comment
                         </p>
                     </div>
                 </div>
                 <div className="flex justify-center my-8 card">
                     <div className="block p-6 rounded-lg shadow-lg border hover:border-doted border-fuchsia-50 bg-purple-400  max-w-sm">
-                        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
-                        <p className="text-gray-700 text-base mb-4">
+                        <h5 className="font-mono text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
+                        <p className="font-mono  text-gray-700 text-base mb-4">
                         Comment
                         </p>
                     </div>

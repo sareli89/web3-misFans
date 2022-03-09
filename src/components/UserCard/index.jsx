d15ef212b3edcae866331ba66f72f7e559731923
Sareli <sareli89@users.noreply.github.com>
2022-03-08 22:45:46 -0500

    img style

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -5,7 +5,7 @@ export const UserCard = ({title, firstName, lastName, picture, onClick}) => {
     return (
         <div onClick={onClick} className="flex items-center border hover:border-doted border-fuchsia-50   bg-purple-400  rounded-lg p-4 m-2 card " >
             <img width="80" className="border border-purple-700 rounded-full mr-4"  src={picture} alt={`Profile photo for ${fullName}`} />
-            <h2 className="text-2xl font-mono text-gray-900 font-bold underline">{title}{" "}
+            <h2 className="text-2xl font-mono text-gray-900 font-bold ">{title}{" "}
             {fullName}
             </h2>
         </div>
--- src/components/UserProfile/index.jsx
+++ src/components/UserProfile/index.jsx
@@ -6,14 +6,15 @@ export  const UserProfile = ({user: {picture, firstName}}) => {
         <React.Fragment>
             <div className="  bg-black ">
                 <div className="flex items-center">
-                    <img width="90" className="rounded-full" src={picture} alt={profilePhotoAlt}/>
+                    <img width="90" className="rounded-full  border-solid md:border-double border-4 border-purple-300" src={picture} alt={profilePhotoAlt}/>
                     <h2 className="text-white text-xl font-semibold mx-8 ">Hola, soy {firstName}</h2>
                 </div>
                 
 
                 <div className="flex justify-center my-8 card">
                     <div className="block p-6 rounded-lg shadow-lg border hover:border-doted border-fuchsia-50 bg-purple-400  max-w-sm">
-                        <h5 className="font-mono text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
+                        <img width="40" className="rounded-full m-2" src={picture} alt={profilePhotoAlt}/>
+                        <h5 className="font-mono text-gray-900 text-2xl leading-tight font-medium mb-2">Autor</h5>
                         <p className="font-mono text-gray-700 text-base mb-4">
                         Comment
                         </p>
@@ -22,6 +23,7 @@ export  const UserProfile = ({user: {picture, firstName}}) => {
 
                 <div className="flex justify-center my-8 card">
                     <div className="block p-6 rounded-lg shadow-lg border hover:border-doted border-fuchsia-50   bg-purple-400  max-w-sm">
+                        <img width="40" className="rounded-full m-2" src={picture} alt={profilePhotoAlt}/>
                         <h5 className="font-mono text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
                         <p className="font-mono text-gray-700 text-base mb-4">
                         Comment
@@ -30,6 +32,7 @@ export  const UserProfile = ({user: {picture, firstName}}) => {
                 </div>
                 <div className="flex justify-center my-8 card">
                     <div className="block p-6 rounded-lg shadow-lg border hover:border-doted border-fuchsia-50 bg-purple-400  max-w-sm">
+                        <img width="40" className="rounded-full m-2" src={picture} alt={profilePhotoAlt}/>
                         <h5 className="font-mono text-gray-900 text-xl leading-tight font-medium mb-2">Autor</h5>
                         <p className="font-mono  text-gray-700 text-base mb-4">
                         Comment

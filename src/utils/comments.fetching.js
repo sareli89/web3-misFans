54d2a15091ffd0556f014ca4035bcc38b872eceb
Sareli <sareli89@users.noreply.github.com>
2022-03-10 01:03:15 -0500

    Move data to environment variables

--- src/utils/comments.fetching.js
+++
@@ -1,9 +0,0 @@
-// export async function getComments () {
-
-//     let commentData;
-//     const headers = {'app-id': '62244f196db59039f13b12fb'};
-//     const comments = fetch('https://dummyapi.io/data/v1/comment?limit=10', {headers});
-//     commentData = await (await comments).json();
-//     return commentData;
-//     console.log(commentData)
-// }
--- src/utils/fetching.js
+++ src/utils/fetching.js
@@ -1,20 +1,19 @@
-const headers = {'app-id': '62244f196db59039f13b12fb'};
-const api_base = 'https://dummyapi.io/data/v1';
-async function getUser () {
-    let userData;
-    const users = fetch(`${api_base}/user?limit=10`, {headers});
-    userData = await (await users).json();
-    return userData;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const TOKEN = process.env.REACT_APP_TOKEN;
 
-
+const headers = { "app-id": TOKEN };
+async function getUser() {
+  let userData;
+  const users = fetch(`${BASE_URL}/user?limit=10`, { headers });
+  userData = await (await users).json();
+  return userData;
 }
 
-async function getComments (userId) {
-    let commentData;
-    const comments = fetch(`${api_base}/user/${userId}/comment`, {headers});
-    commentData = await (await comments).json();
-    return commentData;
-
+async function getComments(userId) {
+  let commentData;
+  const comments = fetch(`${BASE_URL}/user/${userId}/comment`, { headers });
+  commentData = await (await comments).json();
+  return commentData;
 }
 
-export { getUser, getComments}
+export { getUser, getComments };
